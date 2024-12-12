7efd63891a70726f0b559c253873e92ca3e97882
Thiago Hernani <thiago.santos1999@hotmail.com>
2024-12-11 21:55:52 -0300

    Game da memoria

---
+++ src/scripts/engine.js
@@ -0,0 +1,57 @@
+const emojis = [
+    "😸",
+    "😸",
+    "🐵",
+    "🐵",
+    "🐺",
+    "🐺",
+    "🦝",
+    "🦝",
+    "🦁",
+    "🦁",
+    "🐮",
+    "🐮",
+    "🐷",
+    "🐷",
+    "🐭",
+    "🐭"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1))
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMath, 500);
+    }
+}
+
+function checkMath(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Voce venceu")
+    }
+}
